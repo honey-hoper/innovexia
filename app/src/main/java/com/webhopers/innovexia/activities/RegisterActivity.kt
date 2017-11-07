569f6260cc2893fee34c0db8e0788a564e38c112
Honey <honeyhoper21@gmail.com>
2017-11-07 15:04:07 +0530

    updated register activity layout with constraint layout

--- app/src/main/java/com/webhopers/innovexia/activities/RegisterActivity.kt
+++ app/src/main/java/com/webhopers/innovexia/activities/RegisterActivity.kt
@@ -22,7 +22,7 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_register_)
 
         initUI()
 
